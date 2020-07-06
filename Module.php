63bb9d762deab7f3c913486070c9b3f7e46891f9
sash04ek <a.ovcharov@gmail.com>
2020-07-06 16:12:21 +0300

    remove trailing spaces

--- Module.php
+++ Module.php
@@ -9,7 +9,7 @@ namespace Aurora\Modules\DropboxFilestorage;
 
 /**
  * Adds ability to work with Dropbox file storage inside Aurora Files module.
- * 
+ *
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
@@ -22,22 +22,22 @@ class Module extends \Aurora\System\Module\AbstractModule
 	protected static $iStorageOrder = 100;
 	protected $oClient = null;
 	protected $aRequireModules = array(
-		'OAuthIntegratorWebclient', 
+		'OAuthIntegratorWebclient',
 		'DropboxAuthWebclient'
 	);
-	
+
 	protected function issetScope($sScope)
 	{
 		return in_array($sScope, explode(' ', $this->getConfig('Scopes')));
-	}	
-	
+	}
+
 	/***** private functions *****/
 	/**
 	 * Initializes DropBox Module.
-	 * 
+	 *
 	 * @ignore
 	 */
-	public function init() 
+	public function init()
 	{
 		$this->subscribeEvent('Files::GetStorages::after', array($this, 'onAfterGetStorages'));
 		$this->subscribeEvent('Files::GetFile', array($this, 'onGetFile'));
@@ -47,28 +47,28 @@ class Module extends \Aurora\System\Module\AbstractModule
 		$this->subscribeEvent('Files::Delete::after', array($this, 'onAfterDelete'));
 		$this->subscribeEvent('Files::Rename::after', array($this, 'onAfterRename'));
 		$this->subscribeEvent('Files::Move::after', array($this, 'onAfterMove'));
-		$this->subscribeEvent('Files::Copy::after', array($this, 'onAfterCopy')); 
+		$this->subscribeEvent('Files::Copy::after', array($this, 'onAfterCopy'));
 		$this->subscribeEvent('Files::GetFileInfo::after', array($this, 'onAfterGetFileInfo'));
 		$this->subscribeEvent('Files::PopulateFileItem::after', array($this, 'onAfterPopulateFileItem'));
-		
+
 		$this->subscribeEvent('Dropbox::GetSettings', array($this, 'onGetSettings'));
 		$this->subscribeEvent('Dropbox::UpdateSettings::after', array($this, 'onAfterUpdateSettings'));
-		
+
 		$this->subscribeEvent('Files::GetItems::before', array($this, 'CheckUrlFile'));
 		$this->subscribeEvent('Files::UploadFile::before', array($this, 'CheckUrlFile'));
 		$this->subscribeEvent('Files::CreateFolder::before', array($this, 'CheckUrlFile'));
 		$this->subscribeEvent('Files::CheckQuota::after', array($this, 'onAfterCheckQuota'));
 	}
-	
+
 	/**
 	 * Obtains DropBox client if passed $sType is DropBox account type.
-	 * 
+	 *
 	 * @param string $sType Service type.
 	 * @return \Dropbox\Client
 	 */
 	protected function getClient()
 	{
-		
+
 		$oDropboxModule = \Aurora\System\Api::GetModule('Dropbox');
 		if ($oDropboxModule instanceof \Aurora\System\Module\AbstractModule)
 		{
@@ -80,8 +80,8 @@ class Module extends \Aurora\System\Module\AbstractModule
 		else
 		{
 			return false;
-		}		
-		
+		}
+
 		if ($this->oClient === null)
 		{
 			\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
@@ -98,20 +98,20 @@ class Module extends \Aurora\System\Module\AbstractModule
 				$this->oClient = new \Kunnu\Dropbox\Dropbox($oDropboxApp);
 			}
 		}
-		
+
 		return $this->oClient;
-	}	
-	
+	}
+
 	/**
 	 * Write to the $aResult variable information about DropBox storage.
-	 * 
+	 *
 	 * @ignore
 	 * @param array $aData Is passed by reference.
 	 */
 	public function onAfterGetStorages($aArgs, &$mResult)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
-		
+
 		$bEnableDropboxModule = false;
 		$oDropboxModule = \Aurora\System\Api::GetModule('Dropbox');
 		if ($oDropboxModule instanceof \Aurora\System\Module\AbstractModule)
@@ -122,75 +122,76 @@ class Module extends \Aurora\System\Module\AbstractModule
 		{
 			$bEnableDropboxModule = false;
 		}
-		
-		
+
+
 		$oOAuthIntegratorWebclientModule = \Aurora\Modules\OAuthIntegratorWebclient\Module::Decorator();
 		$oOAuthAccount = $oOAuthIntegratorWebclientModule->GetAccount(self::$sStorageType);
 
-		if ($oOAuthAccount instanceof \Aurora\Modules\OAuthIntegratorWebclient\Classes\Account && 
+		if ($oOAuthAccount instanceof \Aurora\Modules\OAuthIntegratorWebclient\Classes\Account &&
 				$oOAuthAccount->Type === self::$sStorageType &&
 					$this->issetScope('storage') && $oOAuthAccount->issetScope('storage'))
-		{		
+		{
 			$mResult[] = [
-				'Type' => self::$sStorageType, 
+				'Type' => self::$sStorageType,
 				'IsExternal' => true,
 				'DisplayName' => 'Dropbox',
-				'Order' => self::$iStorageOrder, 
+				'Order' => self::$iStorageOrder,
 				'IsDroppable' => true
 			];
 		}
 	}
-	
+
 	/**
 	 * Returns directory name for the specified path.
-	 * 
+	 *
 	 * @param string $sPath Path to the file.
 	 * @return string
 	 */
 	protected function getDirName($sPath)
 	{
 		$sPath = dirname($sPath);
-		return str_replace(DIRECTORY_SEPARATOR, '/', $sPath); 
+		return str_replace(DIRECTORY_SEPARATOR, '/', $sPath);
 	}
-	
+
 	/**
 	 * Returns base name for the specified path.
-	 * 
+	 *
 	 * @param string $sPath Path to the file.
 	 * @return string
 	 */
 	protected function getBaseName($sPath)
 	{
 		$aPath = explode('/', $sPath);
-		return end($aPath); 
+		return end($aPath);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param type $oItem
 	 * @return type
 	 */
 	protected function getItemHash($oItem)
 	{
 		return \Aurora\System\Api::EncodeKeyValues(array(
-			'UserId' => \Aurora\System\Api::getAuthenticatedUserId(), 
+			'UserId' => \Aurora\System\Api::getAuthenticatedUserId(),
 			'Type' => $oItem->TypeStr,
 			'Path' => '',
-			'Name' => $oItem->FullPath
-		));			
-	}	
-	
+			'Name' => $oItem->FullPath,
+			'FileName' => $oItem->FileName
+		));
+	}
+
 	protected function hasThumb($sName)
 	{
 		return in_array(
-			pathinfo($sName, PATHINFO_EXTENSION), 
+			pathinfo($sName, PATHINFO_EXTENSION),
 			['jpg', 'jpeg', 'png', 'tiff', 'tif', 'gif', 'bmp']
 		);
 	}
 
 	/**
 	 * Populates file info.
-	 * 
+	 *
 	 * @param string $sType Service type.
 	 * @param \Dropbox\Client $oClient DropBox client.
 	 * @param array $aData Array contains information about file.
@@ -199,12 +200,12 @@ class Module extends \Aurora\System\Module\AbstractModule
 	protected function populateFileInfo($aData)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
-		
+
 		$mResult = false;
 		if ($aData)
 		{
 			$sPath = ltrim($this->getDirName($aData->getPathDisplay()), '/');
-			
+
 			$mResult /*@var $mResult \Aurora\Modules\Files\Classes\FileItem */ = new  \Aurora\Modules\Files\Classes\FileItem();
 			$mResult->IsExternal = true;
 			$mResult->TypeStr = self::$sStorageType;
@@ -221,7 +222,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 //			$mResult->Shared = isset($aData['shared']) ? $aData['shared'] : false;
 			$mResult->FullPath = $mResult->Name !== '' ? $mResult->Path . '/' . $mResult->Name : $mResult->Path ;
 			$mResult->ContentType = \Aurora\System\Utils::MimeContentType($mResult->Name);
-			
+
 			$mResult->Thumb = $this->hasThumb($mResult->Name);
 
 			if ($mResult->IsFolder)
@@ -245,11 +246,11 @@ class Module extends \Aurora\System\Module\AbstractModule
 			}
 		}
 		return $mResult;
-	}	
-	
+	}
+
 	/**
 	 * Writes to the $mResult variable open file source if $sType is DropBox account type.
-	 * 
+	 *
 	 * @ignore
 	 * @param int $iUserId Identifier of the authenticated user.
 	 * @param string $sType Service type.
@@ -266,7 +267,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 			if ($oClient)
 			{
 				$sFullPath = $aArgs['Path'] . '/'  .  ltrim($aArgs['Name'], '/');
-				
+
 				if (isset($aArgs['IsThumb']) && (bool)$aArgs['IsThumb'] === true)
 				{
 					$oThumbnail = $oClient->getThumbnail($sFullPath, 'medium', 'png');
@@ -288,14 +289,14 @@ class Module extends \Aurora\System\Module\AbstractModule
 					}
 				}
 			}
-			
+
 			return true;
 		}
-	}	
-	
+	}
+
 	/**
 	 * Writes to $aData variable list of DropBox files if $aData['Type'] is DropBox account type.
-	 * 
+	 *
 	 * @ignore
 	 * @param array $aData Is passed by reference.
 	 */
@@ -321,8 +322,8 @@ class Module extends \Aurora\System\Module\AbstractModule
 					$oItems = $oListFolderContents->getItems();
 					$aItems = $oItems->all();
 				}
-				
-				foreach($aItems as $oChild) 
+
+				foreach($aItems as $oChild)
 				{
 					if ($oChild instanceof \Kunnu\Dropbox\Models\SearchResult)
 					{
@@ -333,23 +334,23 @@ class Module extends \Aurora\System\Module\AbstractModule
 					{
 						$mResult[] = $oItem;
 					}
-				}				
+				}
 			}
-			
+
 			return true;
 		}
-	}	
+	}
 
 	/**
 	 * Creates folder if $aData['Type'] is DropBox account type.
-	 * 
+	 *
 	 * @ignore
 	 * @param array $aData Is passed by reference.
 	 */
 	public function onAfterCreateFolder($aArgs, &$mResult)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
-		
+
 		if ($aArgs['Type'] === self::$sStorageType)
 		{
 			$oClient = $this->getClient();
@@ -357,7 +358,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 			{
 				$mResult = false;
 				$sPath = $aArgs['Path'];
-				
+
 				if ($oClient->createFolder($sPath.'/'.$aArgs['FolderName']) !== null)
 				{
 					$mResult = true;
@@ -365,18 +366,18 @@ class Module extends \Aurora\System\Module\AbstractModule
 			}
 			return true;
 		}
-	}	
+	}
 
 	/**
 	 * Creates file if $aData['Type'] is DropBox account type.
-	 * 
+	 *
 	 * @ignore
 	 * @param array $aData
 	 */
 	public function onCreateFile($aArgs, &$mResult)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
-		
+
 		if ($aArgs['Type'] === self::$sStorageType)
 		{
 			$oClient = $this->getClient();
@@ -390,29 +391,29 @@ class Module extends \Aurora\System\Module\AbstractModule
 				{
 					$rData = fopen('php://memory','r+');
 					fwrite($rData, $aArgs['Data']);
-					rewind($rData);					
+					rewind($rData);
 				}
 				$oDropboxFile = \Kunnu\Dropbox\DropboxFile::createByStream($aArgs['Name'], $rData);
 				if ($oClient->upload($oDropboxFile,	$Path))
 				{
 					$mResult = true;
 				}
-				
+
 				return true;
 			}
 		}
-	}	
+	}
 
 	/**
 	 * Deletes file if $aData['Type'] is DropBox account type.
-	 * 
+	 *
 	 * @ignore
 	 * @param array $aData
 	 */
 	public function onAfterDelete($aArgs, &$mResult)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
-		
+
 		if ($aArgs['Type'] === self::$sStorageType)
 		{
 			$oClient = $this->getClient();
@@ -428,18 +429,18 @@ class Module extends \Aurora\System\Module\AbstractModule
 			}
 			return true;
 		}
-	}	
+	}
 
 	/**
 	 * Renames file if $aData['Type'] is DropBox account type.
-	 * 
+	 *
 	 * @ignore
 	 * @param array $aData
 	 */
 	public function onAfterRename($aArgs, &$mResult)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
-		
+
 		if ($aArgs['Type'] === self::$sStorageType)
 		{
 			$oClient = $this->getClient();
@@ -454,18 +455,18 @@ class Module extends \Aurora\System\Module\AbstractModule
 				}
 			}
 		}
-	}	
+	}
 
 	/**
 	 * Moves file if $aData['Type'] is DropBox account type.
-	 * 
+	 *
 	 * @ignore
 	 * @param array $aData
 	 */
 	public function onAfterMove($aArgs, &$mResult)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
-		
+
 		if ($aArgs['FromType'] === self::$sStorageType)
 		{
 			$oClient = $this->getClient();
@@ -484,18 +485,18 @@ class Module extends \Aurora\System\Module\AbstractModule
 			}
 			return true;
 		}
-	}	
+	}
 
 	/**
 	 * Copies file if $aData['Type'] is DropBox account type.
-	 * 
+	 *
 	 * @ignore
 	 * @param array $aData
 	 */
 	public function onAfterCopy($aArgs, &$mResult)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
-		
+
 		if ($aArgs['FromType'] === self::$sStorageType)
 		{
 			$oClient = $this->getClient();
@@ -514,8 +515,8 @@ class Module extends \Aurora\System\Module\AbstractModule
 			}
 			return true;
 		}
-	}		
-	
+	}
+
 	protected function _getFileInfo($sPath, $sName)
 	{
 		$mResult = false;
@@ -524,7 +525,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 		{
 			$mResult = $oClient->getMetadata($sPath.'/'.$sName);
 		}
-		
+
 		return $mResult;
 	}
 
@@ -542,7 +543,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 	public function onAfterGetFileInfo($aArgs, &$mResult)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
-		
+
 		if (self::$sStorageType === $aArgs['Type'])
 		{
 			$mFileInfo = $this->_getFileInfo($aArgs['Path'], $aArgs['Id']);
@@ -551,9 +552,9 @@ class Module extends \Aurora\System\Module\AbstractModule
 				$mResult = $this->PopulateFileInfo($mFileInfo);
 			}
 			return true;
-		}		
-	}	
-	
+		}
+	}
+
 	/**
 	 * @ignore
 	 * @todo not used
@@ -564,7 +565,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 	{
 		if ($oItem->IsLink)
 		{
-			if (false !== strpos($oItem->LinkUrl, 'dl.dropboxusercontent.com') || 
+			if (false !== strpos($oItem->LinkUrl, 'dl.dropboxusercontent.com') ||
 					false !== strpos($oItem->LinkUrl, 'dropbox.com'))
 			{
 				$aMetadata = $this->getMetadataLink($oItem->LinkUrl);
@@ -581,13 +582,13 @@ class Module extends \Aurora\System\Module\AbstractModule
 				return true;
 			}
 		}
-	}	
-	
+	}
+
 	protected function getMetadataLink($sLink)
 	{
 		$oClient = $this->getClient();
         $response = $oClient->postToAPI(
-            '/2/sharing/get_shared_link_metadata', 
+            '/2/sharing/get_shared_link_metadata',
 			array(
 				'url' => $sLink
 			)
@@ -600,7 +601,7 @@ class Module extends \Aurora\System\Module\AbstractModule
         if (array_key_exists("is_deleted", $metadata) && $metadata["is_deleted"]) return null;
         return $metadata;
 	}
-	
+
 	public function CheckUrlFile(&$aArgs, &$mResult)
 	{
 		if (strpos($aArgs['Path'], '.url') !== false)
@@ -632,16 +633,16 @@ class Module extends \Aurora\System\Module\AbstractModule
 			$mResult = false;
 			\Aurora\System\Api::GetModuleManager()->broadcastEvent(
 				'Files',
-				'GetFile', 
+				'GetFile',
 				$aArgs,
 				$mResult
-			);	
+			);
 			if (is_resource($mResult))
 			{
 				$aUrlFileInfo = \Aurora\System\Utils::parseIniString(stream_get_contents($mResult));
 				if ($aUrlFileInfo && isset($aUrlFileInfo['URL']))
 				{
-					if (false !== strpos($aUrlFileInfo['URL'], 'dl.dropboxusercontent.com') || 
+					if (false !== strpos($aUrlFileInfo['URL'], 'dl.dropboxusercontent.com') ||
 						false !== strpos($aUrlFileInfo['URL'], 'dropbox.com'))
 					{
 						$aData['Type'] = 'dropbox';
@@ -652,14 +653,14 @@ class Module extends \Aurora\System\Module\AbstractModule
 						}
 					}
 				}
-			}		
+			}
 		}
-	}	
+	}
 	/***** private functions *****/
-	
+
 	/**
 	 * Passes data to connect to service.
-	 * 
+	 *
 	 * @ignore
 	 * @param string $aArgs Service type to verify if data should be passed.
 	 * @param boolean|array $mResult variable passed by reference to take the result.
@@ -667,7 +668,7 @@ class Module extends \Aurora\System\Module\AbstractModule
 	public function onGetSettings($aArgs, &$mResult)
 	{
 		$oUser = \Aurora\System\Api::getAuthenticatedUser();
-		
+
 		if (!empty($oUser))
 		{
 			$aScope = array(
@@ -691,9 +692,9 @@ class Module extends \Aurora\System\Module\AbstractModule
 					$mResult['Scopes'][] = $aScope;
 				}
 			}
-		}	
+		}
 	}
-	
+
 	public function onAfterUpdateSettings($aArgs, &$mResult)
 	{
 		$sScope = '';
